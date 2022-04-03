79ace817f403d5dc3832323897711670b0b90771
carsonbone <carsonbone@utexas.edu>
2022-04-03 01:39:53 -0500

    jkhg

--- frontend/src/InputComponent.js
+++ frontend/src/InputComponent.js
@@ -38,7 +38,7 @@ export default class InputComponent extends React.Component {
                 "Content_Type": "application/json",
                 'Accept': 'application/json'
             },
-            body: JSON.stringify(this.state.value )
+            body: JSON.stringify(this.state.value)
         }
         ).then(response => {
             return response.json()
